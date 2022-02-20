5ca3389d221dc8b367296f94b07fd2d9a36fe0f0
Rimon <saifullahmahmud.hstu@gmail.com>
2022-02-20 14:03:04 +0600

    no 3

--- bin/assignment2.dart
+++ bin/assignment2.dart
@@ -20,7 +20,25 @@ void main(List<String> arguments) {
 
   print(studentlist.length);
 
+  //no 3
 
-  
+  List mylist =[ [1,2,3],{'id':'110','name':'Rimon'}, 35.2,[],{},1,'Hello flutter'];
+
+  print(mylist.length);
+
+  print(mylist);
+
+  //update data
+
+  mylist.add('New Data');
+  print(mylist);
+  mylist.add(01989876);
+  print(mylist);
+
+  //remove at least one ..remove index 2 then print
+
+  mylist.removeAt(2);
+  print(mylist);
+
 
 }
