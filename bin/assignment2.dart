bcbfd57d7ab4b9d3d9143dd2c49ae8a6bd1fd18d
Rimon <saifullahmahmud.hstu@gmail.com>
2022-02-25 15:51:43 +0600

    use separate method for every question

--- bin/assignment2.dart
+++ bin/assignment2.dart
@@ -1,108 +1,22 @@
 void main(List<String> arguments) {
 
   //no 1
-  List newlist =['ABCD','EFGH','IJKL'];
+  myNewMethod0();
 
-  print(newlist);
 
 
   //no 2
 
-  List studentlist =['Rahim','Karim','Abdul','Johan','Babar'];
-
-  print(studentlist);
-
-  print(studentlist.length);
-
-  studentlist.clear();
-  //after clear
-  print(studentlist);
-
-  print(studentlist.length);
+  mynNewMethod1();
 
   //no 3
 
-  List mylist =[ [1,2,3],{'id':'110','name':'Rimon'}, 35.2,[],{},1,'Hello flutter'];
-
-  print(mylist.length);
-
-  print(mylist);
-
-  //update data
-
-  mylist.add('New Data');
-  print(mylist);
-  mylist.add(01989876);
-  print(mylist);
-
-  //remove at least one ..remove index 2 then print
-
-  mylist.removeAt(2);
-  print(mylist);
-
-
+ myNewMethodList();
   //no 4
-  // Write a program to convert string to
-  // double, double to String, int to String, then
-  // write a Heterogeneous list.
+  MyNewMethodFor4();
 
-  //string to double
-  String s = '500';
-  double d = double.parse(s);
-  print(d);
-
-  //double to string
-
-  double d1 = 445.9709;
-  String newstring = d1.toString();
-  print(newstring);
-
-  //int to string
-
-  int i = 234567876;
-  String newstring1 = i.toString();
-  print(newstring1);
-
-  //Heterogeneous List ..Mixed data type
-
-  List list1 =['Rimon',098767,987.990, {'name': 'Rimon'},['XYZ']];
-
-  print(list1);
-
-  //exception handling
-  //a
-
-  try{
-    var a=double.parse('dfsdf');
-    print(a);
-  }
-
-  catch(e,l){
-    print('Exception Name: $e' '  Line at :$l'  );
-  }
-
-  //b and c
-  var list=[];
-  try{
-
-   print(list[0]);
-  }
-
-  catch(e,l){
-    print('Exception Name: $e' '  Line at :$l'  );
-  }
-
-
-
-//d
-  try{
-    int b=int.parse("10.55");
-    print(b);
-  }
-  catch(e,l){
-    print('Exception Name for d : $e'' Line at : $l' );
-  }
-
+  // no 5..
+  exceptionHandling();
 // no 6 ..method call and pass data
 
   var multiplicationprint = myNewMethod(100, 20);
@@ -131,8 +45,123 @@ void main(List<String> arguments) {
 
 //end of main function
 
+void myNewMethod0() {
+  List newlist =['ABCD','EFGH','IJKL'];
+
+  print(newlist);
+
+}
 
 
+
+//no 2
+
+void mynNewMethod1(){
+  List studentlist =['Rahim','Karim','Abdul','Johan','Babar'];
+
+  print(studentlist);
+
+  print(studentlist.length);
+
+  studentlist.clear();
+  //after clear
+  print(studentlist);
+
+  print(studentlist.length);
+
+}
+
+void myNewMethodList(){
+  List<dynamic> myList =[ [1,2,3],{'id':'110','name':'Rimon'}, 35.2,[],{},1,'Hello flutter'];
+
+  print(myList.length);
+
+  print(myList);
+
+  //update data
+
+  myList.add('New Data');
+  print(myList);
+  myList.add(01989876);
+  print(myList);
+
+  //remove at least one ..remove index 2 then print
+
+  myList.removeAt(2);
+  print(myList);
+
+
+}
+
+ void MyNewMethodFor4(){
+   // Write a program to convert string to
+   // double, double to String, int to String, then
+   // write a Heterogeneous list.
+
+   //string to double
+   String s = '500';
+   double d = double.parse(s);
+   print(d);
+
+   //double to string
+
+   double d1 = 445.9709;
+   String newstring = d1.toString();
+   print(newstring);
+
+   //int to string
+
+   int i = 234567876;
+   String newstring1 = i.toString();
+   print(newstring1);
+
+   //Heterogeneous List ..Mixed data type
+
+   List list1 =['Rimon',098767,987.990, {'name': 'Rimon'},['XYZ']];
+
+   print(list1);
+
+
+
+
+ }
+
+//no 5.
+//exception handling
+//a
+  void exceptionHandling(){
+
+    try{
+      var a=double.parse('dfsdf');
+      print(a);
+    }
+
+    catch(e,l){
+      print('Exception Name: $e' '  Line at :$l'  );
+    }
+
+    //b and c
+    var list=[];
+    try{
+
+      print(list[0]);
+    }
+
+    catch(e,l){
+      print('Exception Name: $e' '  Line at :$l'  );
+    }
+
+
+
+//d
+    try{
+      int b=int.parse("10.55");
+      print(b);
+    }
+    catch(e,l){
+      print('Exception Name for d : $e'' Line at : $l' );
+    }
+  }
 // no 6
  int myNewMethod(int value1 , int value2){
 
